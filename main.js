8c8561461e8e45d7266b67b4e7afbc511dfdfa37
Ricardo Palacios <ricardopalacios2526@gmail.com>
2024-07-10 23:12:51 -0600

    ejercicio 6 completo

---
+++ main.js
@@ -0,0 +1,144 @@
+// //1
+
+// let numeroA = 3
+// let numeroB = 2
+
+// if(numeroA > numeroB){
+//   console.log("el primer numero es mayor que el segundo")
+// }
+
+// //2
+
+// let numeroA = 2
+// let numeroB = 2
+
+// if(numeroA === numeroB){
+//   console.log("los numeros son iguales")
+// } else{
+//   console.log("los numeros son diferentes")
+// }
+
+// //3
+
+// let numeroA = 1
+// let numeroB = 2
+
+// if(numeroA < numeroB ){
+//   console.log("numeroB es mayor")
+// } else if(numeroA > numeroB){
+//   console.log("numeroA es mayor")
+// }else{
+//   console.log("los numeros son iguales")
+// } 
+
+// //4
+
+// let numeroA = 4
+// let numeroB = 8
+// let numeroC = 1
+
+// if (numeroA < numeroB) {
+//   if (numeroA < numeroC) {
+//     alert("numeroA es el numero mas chico")
+//   } else {
+//     alert("numeroC es el numero mas chico")
+//   }
+// } else {
+//   if (numeroB < numeroC) {
+//     alert("numeroB es el numero mas chico")
+//   } else {
+//     alert("numeroC es el numero mas chico")
+//   }
+// }
+
+//5
+
+// let persona1 = {
+//   nombre : "Javier",
+//   edad : 25,
+//   altura : 185
+// }
+
+// let persona2 = {
+//   nombre : "Fernando",
+//   edad : 26,
+//   altura : 170
+// }
+
+// if (persona1.edad > persona2.edad) {
+// alert("persona1 es la persona con mas edad ");
+// }else{
+//   if (persona1.edad < persona2.edad){
+//     alert("persona2 es la persona con mas edad ");
+//   }
+// }
+
+// if (persona1.altura > persona2.altura) {
+// alert("persona1 es la persona con mas altura ");
+// }else{
+//   if (persona1.altura < persona2.altura){
+//     alert("persona2 es la persona con mas altura ");
+//   }
+// }
+
+
+// //6
+
+// let nombre = prompt("ingresa tu nombre")
+// let edad = parseInt(prompt("ingresa tu edad"))
+
+// //Edad
+
+// if (edad >= 18) {
+//   alert("cumple con el requisito: Mayor de edad")
+
+//   //Altura
+
+//   let altura = parseInt(prompt("ingresa tu altura"))
+//   if (altura >= 150) {
+//     alert("cumple con el requisito: Medir mas de 150 cm")
+
+//     //Grado de vision
+
+//     let vision = parseInt(prompt("ingresa tu grado de vision entre 1 y 10"))
+//     if ((vision => 8) && (vision <= 10)) {
+//       alert("cumple con el requisito: Grado de vision")
+//     } else {
+//       alert("No cumple con el requisito: Grado de vision")
+//     }
+//   }
+//   else
+//     if (altura < 150) {
+//       alert("No cumple con el requisito: Medir mas de 150 cm")
+
+//     }
+
+
+// } else {
+//   if (edad < 18) {
+//     alert("No cumple con el requisito: Mayor de edad")
+//   }
+// }
+
+//7
+
+alert ("Bienvenido!")
+let nombre = prompt("ingresa tu nombre")
+let pase = prompt("ingresa tu tipo de pase (vip o normal)")
+let entrada = prompt("¿Posees una entrada ( s, o, n)?")
+let usarEntrada = "si"
+let vip = "vip" == "Ricardo"
+let s = true
+let n = false
+
+if (entrada === "s") {
+  alert("¿Deseas utilizar tu entrada?")
+}if () {
+  
+}
+
+
+
+
+
+
